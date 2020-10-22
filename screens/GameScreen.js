590d716085ef152c597b29f4bc8aae2c576b26df
nicholasgalante1997 <nicholasgalante1997@gmail.com>
2020-10-22 01:37:24 -0400

    gamescreen logic

---
+++ screens/GameScreen.js
@@ -0,0 +1,26 @@
+import React, {useState} from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+
+const generateRandomBetween = (min, max, exclude) => {
+    min = Math.ceil(min)
+    max = Math.floor(max)
+    const randomNumber = (Math.random() * (max - min)) + min;
+    if (randomNumber === exclude) {
+       return generateRandomBetween(min, max, exclude)
+    } else {
+        return randomNumber;
+    }
+}
+
+const GameScreen = (props) => {
+    const [currentGuess, setCurrentGuess] = useState(generateRandomBetween(1, 100, props.userChoice))
+    return (
+        
+     );
+}
+ 
+const styles = StyleSheet.create({
+
+})
+
+export default GameScreen;
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,23 +1,83 @@
-import React from 'react';
-import {View, Text, StyleSheet, TextInput, Button} from 'react-native'
+import React, {useState} from 'react';
+import {View, 
+    Text, 
+    StyleSheet, 
+    Button, 
+    TouchableWithoutFeedback,
+    Keyboard,
+    Alert} 
+from 'react-native'
+
 import Card from '../components/Card'
 import Input from '../components/Input'
 import Colors from '../constants/colors'
+import NumberContainer from '../components/NumberContainer'
 
 
-const StartGameScreen = () => {
+const StartGameScreen = (props) => {
+
+    const [enteredValue, setEnteredValue] = useState('')
+    const [confirmed, setConfirmed] = useState(false)
+    const [selectedNumber, setSelectedNumber] = useState()
+
+    const numberInputHandler = (inputText) => {
+        setEnteredValue(inputText.replace(/[^0-9]/g, ""))
+    }
+
+    const resetInputHandler = () => {
+        setEnteredValue("")
+        setConfirmed(false)
+    }
+
+    const confirmInputHandler = () => {
+        const chosenNumber = parseInt(enteredValue)
+        if (isNaN(chosenNumber)|| chosenNumber <= 0 || chosenNumber > 99) {
+            return Alert.alert("Invalid Number!", "Number has to be a number between one and ninety-nine.", [{text: 'Okay?', style: 'destructive', onPress: resetInputHandler }]);
+        } 
+        setConfirmed(true)
+        setSelectedNumber(parseInt(chosenNumber))
+        setEnteredValue('')
+        Keyboard.dismiss()
+    }
+
+    let confirmedOutput
+
+    if (confirmed) {
+        confirmedOutput = 
+        <Card style={styles.summaryContainer}>
+            <Text>You Selected</Text>
+            <NumberContainer>{selectedNumber}</NumberContainer>
+            <Button title="Start Game" />
+        </Card>
+    }
     return ( 
+        <TouchableWithoutFeedback onPress={() => {Keyboard.dismiss()}}>
         <View style={styles.screen}>
             <Text style={styles.title}>Start a New Game</Text>
             <Card style={styles.inputContainer}>
                 <Text>Enter a Number</Text>
-                <Input style={styles.input}/>
+                <Input style={styles.input} 
+                blurOnSubmit 
+                autoCapitalize="none" 
+                autoCorrect={false} 
+                keyboardType="number-pad" 
+                maxLength={2} 
+                onChangeText={numberInputHandler}
+                value={enteredValue} />
                 <View style={styles.buttonContainer}>
-                   <View style={styles.button}><Button title="Reset" color={Colors.accent}/></View> 
-                   <View style={styles.button}><Button title="Confirm" color={Colors.primary}/></View> 
+                   <View style={styles.button}>
+                       <Button title="Reset" color={Colors.accent} 
+                       onPress={resetInputHandler}/>
+                    </View> 
+                   <View style={styles.button}>
+                       <Button title="Confirm" color={Colors.primary}
+                       onPress={confirmInputHandler} />
+                    </View> 
                 </View>
             </Card>
+            {confirmedOutput}
         </View>
+        </TouchableWithoutFeedback>
      );
 }
  
@@ -49,6 +109,10 @@ const styles = StyleSheet.create({
     input: {
         width: 50,
         textAlign: 'center'
+    },
+    summaryContainer: {
+        marginTop: 20,
+        alignItems: 'center'
     }
 })
 
