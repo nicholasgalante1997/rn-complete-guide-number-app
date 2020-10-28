6e87cccaed04206671a41ea759f8519087f8bfbc
nicholasgalante1997 <nicholasgalante1997@gmail.com>
2020-10-28 15:24:26 -0400

    final-sub

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Platform} from 'react-native';
 import Colors from '../constants/colors'
 import DefaultStyles from '../constants/default-styles'
 
@@ -16,12 +16,15 @@ const styles = StyleSheet.create({
         width: "100%",
         height: 90,
         paddingTop: 36,
-        backgroundColor: Colors.primary,
+        backgroundColor: Platform.OS === 'android' ? Colors.primary : 'white',
         alignItems: 'center',
-        justifyContent: 'center'
+        justifyContent: 'center',
+        borderBottomColor: Platform.OS === 'ios' ? '#ccc' : 'transparent',
+        borderBottomWidth: Platform.OS === 'ios' ? 1 : 0,
+
     },
     headerTitle: {
-        color: 'black',
+        color: Platform.OS === 'ios' ? Colors.primary : 'white',
         fontSize: 18,
         fontFamily: 'open-sans-bold'
     }
--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -7,6 +7,8 @@ import MainButton from '../components/MainButton';
 import BodyText from '../components/BodyText'
 
 import { Ionicons } from '@expo/vector-icons'
+import * as ScreenOrientation from 'expo-screen-orientation'
+
 
 const generateRandomBetween = (min, max, exclude) => {
     min = Math.ceil(min)
@@ -27,6 +29,9 @@ const renderListItem = (value, numOfRounds) => {
 }
 
 const GameScreen = (props) => {
+
+    // locking a specific screen
+    // ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.PORTRAIT)
 
     const initialGuess = generateRandomBetween(1, 100, props.userChoice)
     const [currentGuess, setCurrentGuess] = useState(initialGuess)
